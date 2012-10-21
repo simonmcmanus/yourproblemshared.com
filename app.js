81d63fc107bd3b377b5beb9c9ed21d2f5bafdc20
Simon McManus <mcmanus.simon@gmail.com>
2012-10-21 18:01:15 +0100

    df

--- app.js
+++ app.js
@@ -45,14 +45,14 @@ app.use(urls.PUBLIC, express['static'](__dirname + '/public/assets/'));
 
 app.get(urls.EMAIL, function(req, res, next) {
     ds.getEmail({
-        id: req.params.id
+        id: req.params.id,
     }, function(error, data) {
-        console.log(arguments) ;
         res.render('email.ejs', {  
             mail: data,
             selected: '',
             hideNav: false, 
-             encoder: encoder.encoder});
+             encoder: encoder.encoder
+        });
     });
 });
 
@@ -124,87 +124,6 @@ var sendEmail = function(to, subject, body) {
 
 app.post(urls.INBOUND, function(req, res, next) {
 
-// req.body = {
-//   "From": "myUser@theirDomain.com",
-//   "FromFull": {
-//     "Email": "myUser@theirDomain.com",
-//     "Name": "John Doe"
-//   },
-//   "To": "451d9b70cf9364d23ff6f9d51d870251569e+ahoy@inbound.postmarkapp.com",
-//   "ToFull": [
-//     {
-//       "Email": "451d9b70cf9364d23ff6f9d51d870251569e+ahoy@inbound.postmarkapp.com",
-//       "Name": ""
-//     }
-//   ],
-//   "Cc": "\"Full name\" <sample.cc@emailDomain.com>, \"Another Cc\" <another.cc@emailDomain.com>",
-//   "CcFull": [
-//     {
-//       "Email": "sample.cc@emailDomain.com",
-//       "Name": "Full name"
-//     },
-//     {
-//       "Email": "another.cc@emailDomain.com",
-//       "Name": "Another Cc"
-//     }
-//   ],
-//   "ReplyTo": "myUsersReplyAddress@theirDomain.com",
-//   "Subject": "This is an inbound message",
-//   "MessageID": "22c74902-a0c1-4511-804f2-341342852c90",
-//   "Date": "Thu, 5 Apr 2012 16:59:01 +0200",
-//   "MailboxHash": "ahoy",
-//   "TextBody": "[ASCII]",
-//   "HtmlBody": "[HTML(encoded)]",
-//   "Tag": "",
-//   "Headers": [
-//     {
-//       "Name": "X-Spam-Checker-Version",
-//       "Value": "SpamAssassin 3.3.1 (2010-03-16) onrs-ord-pm-inbound1.wildbit.com"
-//     },
-//     {
-//       "Name": "X-Spam-Status",
-//       "Value": "No"
-//     },
-//     {
-//       "Name": "X-Spam-Score",
-//       "Value": "-0.1"
-//     },
-//     {
-//       "Name": "X-Spam-Tests",
-//       "Value": "DKIM_SIGNED,DKIM_VALID,DKIM_VALID_AU,SPF_PASS"
-//     },
-//     {
-//       "Name": "Received-SPF",
-//       "Value": "Pass (sender SPF authorized) identity=mailfrom; client-ip=209.85.160.180; helo=mail-gy0-f180.google.com; envelope-from=myUser@theirDomain.com; receiver=451d9b70cf9364d23ff6f9d51d870251569e+ahoy@inbound.postmarkapp.com"
-//     },
-//     {
-//       "Name": "DKIM-Signature",
-//       "Value": "v=1; a=rsa-sha256; c=relaxed\/relaxed;        d=wildbit.com; s=google;        h=mime-version:reply-to:date:message-id:subject:from:to:cc         :content-type;        bh=cYr\/+oQiklaYbBJOQU3CdAnyhCTuvemrU36WT7cPNt0=;        b=QsegXXbTbC4CMirl7A3VjDHyXbEsbCUTPL5vEHa7hNkkUTxXOK+dQA0JwgBHq5C+1u         iuAJMz+SNBoTqEDqte2ckDvG2SeFR+Edip10p80TFGLp5RucaYvkwJTyuwsA7xd78NKT         Q9ou6L1hgy\/MbKChnp2kxHOtYNOrrszY3JfQM="
-//     },
-//     {
-//       "Name": "MIME-Version",
-//       "Value": "1.0"
-//     },
-//     {
-//       "Name": "Message-ID",
-//       "Value": "<CAGXpo2WKfxHWZ5UFYCR3H_J9SNMG+5AXUovfEFL6DjWBJSyZaA@mail.gmail.com>"
-//     }
-//   ],
-//   "Attachments": [
-//     {
-//       "Name": "myimage.png",
-//       "Content": "[BASE64-ENCODED CONTENT]",
-//       "ContentType": "image/png",
-//       "ContentLength": 4096
-//     },
-//     {
-//       "Name": "mypaper.doc",
-//       "Content": "[BASE64-ENCODED CONTENT]",
-//       "ContentType": "application/msword",
-//       "ContentLength": 16384
-//     }
-//   ]
-// };
     var sortHeaders = function(headers) {
         var obj = {};
         if(!headers) {
@@ -227,13 +146,13 @@ app.post(urls.INBOUND, function(req, res, next) {
     console.log('sending: ', req.body);
     ds.saveEmail({
         id: req.body.MessageID,
-        toEmail: req.body.ToFull.Email || 'No Email',
-        toName: req.body.ToFull.Name || 'No Name',
-        fromEmail: req.body.FromFull.Email || 'No from email',
-        replyTo: req.body.FromFull.replyTo || 'No reply to',
-        fromName: req.body.ToFull.Name || 'No from name',
-        ccEmail: req.body.CcFull.Email  || 'No cc email',
-        ccName: req.body.CcFull.Email  || 'No cc name',
+        toEmail: req.body.ToFull[0].Email,
+        toName: req.body.ToFull[0].Name,
+        fromEmail: req.body.FromFull[0].Email,
+        replyTo: req.body.FromFull.replyTo,
+        fromName: req.body.FromFull.Name,
+        ccEmail: req.body.CcFull.Email,
+        ccName: req.body.CcFull.Email,
         subject: req.body.Subject,
         textBody:  req.body.TextBody,
         htmlBody:  req.body.HtmlBody,
