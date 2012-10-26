abff24e2575eb111a536e3b9e3b6321583a65688
Simon McManus <mcmanus.simon@gmail.com>
2012-10-26 14:18:33 +0100

    df

--- app.js
+++ app.js
@@ -186,12 +186,11 @@ app.post(urls.INBOUND, function(req, res, next) {
         if(data) {
             if(+isFirst) {
                 var url = 'http://yourproblemshared.com/'+site+'/mail/'+data.insertId+'/';
-
                 fs.readFile('./views/emails/user-problem-reported.html', 'utf8', function(error, data) {
                     console.log(data);
                     var body = ejs.render(data, {
-                        site: site,
-                         url: url
+                        site: 'Google',
+                         url: 'http://google.com'
                     });
                     sendEmail(req.body.From, 'Relax, your problem has been shared', body);
                 });
@@ -200,8 +199,8 @@ app.post(urls.INBOUND, function(req, res, next) {
                 fs.readFile('./views/emails/site-problem-reported.html', 'utf8', function(error, data) {
                     console.log(data);
                     var body = ejs.render(data, {
-                        site: site,
-                         url: url
+                        site: 'Google',
+                         url: 'http://google.com'
                     });
                     sendEmail(req.body.From, 'ACTION REQUIRED', body);
                 });
