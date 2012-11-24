28c4c3036c6f5a7e61be97d7c7d42f2ecc2b2038
Simon McManus <mcmanus.simon@gmail.com>
2012-11-24 18:10:25 +0000

    working

--- app.js
+++ app.js
@@ -167,14 +167,10 @@ app.get(urls.EMAIL, function(req, res, next) {
         id: req.params.id,
     }, function(replies) {
 
-//        replies.push(email);
-//        console.lo
-//        
-console.log('>>', replies);
-        if(replies[0].resolved === 1) {
+        if(replies && replies[0] && replies[0].resolved === 1) {
             var msg = '<strong>Resolved  '+moment(+replies[0].resolvedEpoch).fromNow()+'</strong>'
         }else {
-            var msg = 'Reported to <strong><a href="/'+replies[0].companyUrl+'">'+replies[0].companyName+'</a></strong> '+moment(new Date(unescape(replies[0].date))).fromNow()+' and remains <strong>UNRESOLVED</strong> '
+            var msg = 'Reported to <strong><a href="/'+( replies[0].companyUrl || replies[0].company)+'">'+(replies[0].companyName || replies[0].company )+'</a></strong> '+moment(new Date(unescape(replies[0].date))).fromNow()+' and remains <strong>UNRESOLVED</strong> '
         }
         res.render('email.ejs', {
             mail: replies,
